3d3d9865b5a2d4aaf5ea0e36f49a5a72c1547157
Giovanni Sambonino <87047313+GiovanniSambonino@users.noreply.github.com>
2023-08-03 18:33:11 -0500

    Update Producto.java

--- TallerMalosOlores_Refactoring/src/main/java/com/espol/tallermalosolores_refactoring/Producto.java
+++ TallerMalosOlores_Refactoring/src/main/java/com/espol/tallermalosolores_refactoring/Producto.java
@@ -1,3 +1,5 @@
+import com.espol.tallermalosolores_refactoring.TipoProducto;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
