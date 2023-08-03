579d5bbbc550ff4a1ac7d7db2524bbae3831c78c
Giovanni Sambonino <87047313+GiovanniSambonino@users.noreply.github.com>
2023-08-03 18:34:33 -0500

    Add files via upload

---
+++ TallerMalosOlores_Refactoring/src/main/java/com/espol/tallermalosolores_refactoring/TipoProducto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.espol.tallermalosolores_refactoring;
+
+/**
+ *
+ * @author Torremar
+ */
+public enum TipoProducto {
+    Fragil, Regular, Pesado
+}
